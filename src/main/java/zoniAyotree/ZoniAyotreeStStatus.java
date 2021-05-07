0b4c136ea0f2cbd73817387103f4860363576fe6
NYerlan <n.yerlan@gmail.com>
2021-05-06 20:56:23 -0700

    fixed test by relocating WebDriverManager from @BeforeClass to @BeforeTest

--- src/main/java/zoniAyotree/ZoniAyotreeStStatus.java
+++ src/main/java/zoniAyotree/ZoniAyotreeStStatus.java
@@ -1,45 +1,64 @@
 package zoniAyotree;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 
 public class ZoniAyotreeStStatus {
-    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "D:/nyerlan/Downloads/chromedriver_win32/chromedriver.exe");
+    @BeforeClass
+    public void before() {
+//        WebDriverManager.chromedriver().setup();
+    }
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://ayotree.com/HomepageTemplateMaster/Login/default.aspx?menu=&submenu=&MasterTeacherID=7198");
+    private WebDriver driver;
 
-        WebElement inputLogin = driver.findElement(By.xpath("//*[@id=\"txtLogin\"]"));
-        inputLogin.sendKeys("ynurgaliyev");
-        Thread.sleep(3000);
+    @BeforeTest
+    public void beforeTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
 
-        WebElement inputPassword = driver.findElement(By.xpath("//*[@id=\"txtPassword\"]"));
-        inputPassword.sendKeys("Zoni2020");
-        Thread.sleep(3000);
+    @Test
+    public void ZoniTest() throws InterruptedException {
 
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"btnLogin\"]"));
-        loginButton.click();
-        Thread.sleep(5000);
+            driver.get("https://ayotree.com/HomepageTemplateMaster/Login/default.aspx?menu=&submenu=&MasterTeacherID=7198");
 
-        driver.get("https://ayotree.com/MainMaster/StudentListEdit.aspx?menu=6&submenu=62");
+            WebElement inputLogin = driver.findElement(By.xpath("//*[@id=\"txtLogin\"]"));
+            inputLogin.sendKeys("ynurgaliyev");
+            Thread.sleep(3000);
 
-        WebElement inputStId = driver.findElement(By.xpath("//*[@id=\"txtNameEmail\"]"));
-        inputStId.sendKeys("84182");
-        inputStId.sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
+            WebElement inputPassword = driver.findElement(By.xpath("//*[@id=\"txtPassword\"]"));
+            inputPassword.sendKeys("Zoni2020");
+            Thread.sleep(3000);
 
-        driver.findElement(By.xpath("//*[@id=\"haEditLink\"]")).click();
+            WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"btnLogin\"]"));
+            loginButton.click();
+            Thread.sleep(5000);
 
-        Thread.sleep(5000);
+            driver.get("https://ayotree.com/MainMaster/StudentListEdit.aspx?menu=6&submenu=62");
 
+            WebElement inputStId = driver.findElement(By.xpath("//*[@id=\"txtNameEmail\"]"));
+            inputStId.sendKeys("84182");
+            inputStId.sendKeys(Keys.ENTER);
+            Thread.sleep(5000);
 
+            driver.findElement(By.xpath("//*[@id=\"haEditLink\"]")).click();
 
+            Thread.sleep(5000);
+
+    }
+    @AfterTest
+    public void afterTest() {
         driver.quit();
     }
 }
